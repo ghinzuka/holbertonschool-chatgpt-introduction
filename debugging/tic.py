9e42dcb6fd9c966d5f8049b0370330e516612325
ghinzuka <baptiste.pouquerou@gmail.com>
2024-05-06 11:25:54 +0200

    trying a way to crrect pycodestyle 2

--- debugging/tic.py
+++ debugging/tic.py
@@ -12,7 +12,7 @@ def check_winner(board):
 
     for col in range(len(board[0])):
         if (board[0][col] == board[1][col] == board[2][col] and
-            board[0][col] != " "):
+            	board[0][col] != " "):
             return True
 
     if board[0][0] == board[1][1] == board[2][2] and board[0][0] != " ":
